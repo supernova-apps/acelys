469685d0f833340ee54233ba5ed179a0b080a56f
luciano <lucianofa.13@outlook.com>
2019-07-19 14:44:55 -0500

    imagenes

--- src/app/advertisement/advertisement.page.ts
+++ src/app/advertisement/advertisement.page.ts
@@ -1,22 +1,105 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ChangeDetectorRef } from "@angular/core";
 
-import { Router } from '@angular/router';
-import { Location } from '@angular/common';
+import { Router } from "@angular/router";
+import { Location } from "@angular/common";
+
+import {
+  Camera,
+  CameraOptions,
+  PictureSourceType
+} from "@ionic-native/Camera/ngx";
+import {
+  ActionSheetController,
+  ToastController,
+  Platform,
+  LoadingController
+} from "@ionic/angular";
+import { File, FileEntry } from "@ionic-native/File/ngx";
+import { HttpClient } from "@angular/common/http";
+import { WebView } from "@ionic-native/ionic-webview/ngx";
+import { Storage } from "@ionic/storage";
+import { FilePath } from "@ionic-native/file-path/ngx";
+
+const STORAGE_KEY = "my_images";
+declare var cordova: any;
 @Component({
-  selector: 'app-advertisement',
-  templateUrl: './advertisement.page.html',
-  styleUrls: ['./advertisement.page.scss'],
+  selector: "app-advertisement",
+  templateUrl: "./advertisement.page.html",
+  styleUrls: ["./advertisement.page.scss"]
 })
 export class AdvertisementPage implements OnInit {
+  images = [];
+  image: any = "";
+  constructor(
+    private router: Router,
+    private location: Location,
+    private camera: Camera,
+    private file: File,
+    private http: HttpClient,
+    private webview: WebView,
+    private actionSheetController: ActionSheetController,
+    private toastController: ToastController,
+    private storage: Storage,
+    private plt: Platform,
+    private loadingController: LoadingController,
+    private ref: ChangeDetectorRef,
+    private filePath: FilePath
+  ) {}
 
-  constructor(private router: Router, private location: Location) { }
+  ngOnInit() {}
 
-  ngOnInit() {
+  selectImage(num) {
+    if (num === 1) {
+      this.pictureFromCamera();
+    } else {
+      this.openGallery();
+    }
+  }
+  async openGallery() {
+    let cameraOptions = {
+      sourceType: 0,
+      destinationType: this.camera.DestinationType.DATA_URL,
+      quality: 100,
+      targetWidth: 1000,
+      targetHeight: 1000,
+      encodingType: this.camera.EncodingType.JPEG,
+      correctOrientation: true
+    };
+
+    const result = await this.camera.getPicture(cameraOptions);
+
+    this.images.push({ path: `data:image/jpeg;base64,${result}` });
+  }
+  async pictureFromCamera() {
+    const options: CameraOptions = {
+      quality: 100,
+      destinationType: this.camera.DestinationType.DATA_URL,
+      encodingType: this.camera.EncodingType.JPEG,
+      mediaType: this.camera.MediaType.PICTURE,
+      correctOrientation: true,
+      saveToPhotoAlbum: true
+    };
+
+    this.takePhoto(options);
+  }
+
+  async takePhoto(options: CameraOptions) {
+    try {
+      const result = await this.camera.getPicture(options);
+
+      this.images.push({ path: `data:image/jpeg;base64,${result}` });
+      // console.log(this.image);
+    } catch (e) {
+      console.error(e);
+    }
+  }
+  deleteImage(img, pos) {
+    this.images.splice(pos, 1);
   }
   back() {
     this.location.back();
   }
   save() {
-    this.router.navigateByUrl('/incidents/tohandle');
+    this.router.navigateByUrl("/incidents/tohandle");
   }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,14 +11,28 @@ import { AppRoutingModule } from './app-routing.module';
 import { ModalPageModule } from './modal/modal.module';
 import { PopoverPageModule } from './popover/popover.module';
 
+import { HttpClientModule } from '@angular/common/http';
+
+import { Camera } from '@ionic-native/Camera/ngx';
+import { File } from '@ionic-native/File/ngx';
+import { WebView } from '@ionic-native/ionic-webview/ngx';
+import { FilePath } from '@ionic-native/file-path/ngx';
+
+import { IonicStorageModule } from '@ionic/storage';
 
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule, ModalPageModule, PopoverPageModule],
+  imports: [
+    BrowserModule, IonicModule.forRoot(), AppRoutingModule, ModalPageModule, PopoverPageModule, HttpClientModule,
+    IonicStorageModule.forRoot()],
   providers: [
     StatusBar,
     SplashScreen,
+    Camera,
+    File,
+    WebView,
+    FilePath,
     { provide: RouteReuseStrategy, useClass: IonicRouteStrategy }
   ],
   bootstrap: [AppComponent]
